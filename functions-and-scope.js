2503f17cec1eb2ed85f7c3fd0d272847d04129a8
kkeulemans <78099351+kkeulemans@users.noreply.github.com>
2022-01-21 16:07:48 +0100

    done

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -129,7 +129,13 @@ console.log(outcome);
 
 // ---- Verwachte uitkomst: 9
 
-
+let highestGrade = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > highestGrade) {
+        highestGrade = grades[i]
+    }
+}
+console.log(highestGrade);
 /* 3b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
